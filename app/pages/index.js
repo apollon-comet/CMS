14f38d244b4d07f423109ff5239cdbd839382b30
Wong Ho <wongho@gmail.com>
2023-09-19 21:54:43 +0800

    initial

---
+++ app/pages/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    if (text.trim().length === 0) return;
+
+    setTodos([...todos, text]);
+    setText("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
